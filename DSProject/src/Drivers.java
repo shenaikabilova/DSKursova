dd6617a1da1ae09dc578fe4ea9fef2024267e087
shenaikabilova <shenito93@abv.bg>
2015-10-24 17:17:59 +0300

    changes

---
+++ DSProject/src/Drivers.java
@@ -0,0 +1,30 @@
+/**
+ * @author shenaikabilova
+ *
+ */
+
+public class Drivers {
+	
+	private String driverFirstName;
+	private String driverLastName;
+	private String driverLicense;
+	
+	public String getDriverFirstName() {
+		return driverFirstName;
+	}
+	public void setDriverFirstName(String driverFirstName) {
+		this.driverFirstName = driverFirstName;
+	}
+	public String getDriverLastName() {
+		return driverLastName;
+	}
+	public void setDriverLastName(String driverLastName) {
+		this.driverLastName = driverLastName;
+	}
+	public String getDriverLicense() {
+		return driverLicense;
+	}
+	public void setDriverLicense(String driverLicense) {
+		this.driverLicense = driverLicense;
+	}
+}
